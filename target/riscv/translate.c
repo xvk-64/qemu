fb6f3fc2026a579c95c785dfc51c19db2ce265ae
valen-CURE <18544814+SpiccyMayonnaise@users.noreply.github.com>
2025-02-11 15:34:27 +1030

    Add customnop instruction

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -1149,6 +1149,7 @@ static uint32_t opcode_at(DisasContextBase *dcbase, target_ulong pc)
 #include "decode-xthead.c.inc"
 #include "insn_trans/trans_xthead.c.inc"
 #include "insn_trans/trans_xventanacondops.c.inc"
+#include "insn_trans/trans_custom.c.inc"
 
 /* Include the auto-generated decoder for 16 bit insn */
 #include "decode-insn16.c.inc"
